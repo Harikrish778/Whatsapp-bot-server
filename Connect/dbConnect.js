e81851bd1a7e41123cb2dc03c942d34570255c91
Harikrish <harikrishnanfeb2003@gmail.com>
2025-08-21 20:11:04 +0530

    second commit

--- Connect/dbConnect.js
+++ Connect/dbConnect.js
@@ -1,7 +1,7 @@
-const mongoose=require('mongoose')
+// const mongoose=require('mongoose')
 
-mongoose.connect(process.env.DBCONNECT).then(res=>{
-    console.log("DB Connected");
-}).catch((err)=>{
-    console.log(err);
-})
+// mongoose.connect(process.env.DBCONNECT).then(res=>{
+//     console.log("DB Connected");
+// }).catch((err)=>{
+//     console.log(err);
+// })
